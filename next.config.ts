e739a0e3a97519b5edaab79a684ae9d03ee280f1
MikeFolepe <davyfolepe70@gmail.com>
2025-11-20 15:31:08 -0500

    handled missing asset

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'mike-folepe';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
